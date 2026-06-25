8bd52c9a82bca57b50fd19ccb02a704eb39c656e
Dxrk548 <hernandezruizjosemanuel365@gmail.com>
2026-06-25 15:45:45 -0600

    Correccion de vulnerabilidades

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from email_sender import enviar_correo_inseguro
 
 app = Flask(__name__)
@@ -8,12 +8,12 @@ def enviar_alerta():
     try:
         enviar_correo_inseguro(
             destinatario="doctor_asesor@correo.com",
-            asunto="Alerta",
-            mensaje="Correo de prueba."
+            asunto="Alerta Critica de Servidor",
+            mensaje="Este es un correo de prueba enviado a traves de un canal inseguro."
         )
         return jsonify({"status": "success", "message": "Correo enviado al SMTP local"}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False)
